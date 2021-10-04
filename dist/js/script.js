20f8ab5069158664018b9c5f37ab6fb2ab568d11
artem <a.omukhov@gmail.com>
2021-10-04 20:47:30 +0300

    feat: Create cards

--- dist/js/script.js
+++ dist/js/script.js
@@ -145,4 +145,85 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    // Карточки
+
+    class MenuCard {                                                            // Класс карточки
+        constructor(src, alt, title, description, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 76;
+            this.changeToRubles();
+        }
+
+        changeToRubles() {                                                      // Конвертация в рубли
+            this.price = this.price * this.transfer;
+        }
+
+        render() {                                                              // Рендер верстки карточки
+            const element = document.createElement('div');
+            
+            if (this.classes.length === 0) {                                    // Если массив классов пустой
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => {
+                    element.classList.add(className);
+                });
+            }
+                                                                                // Верстка карточки
+            element.innerHTML = `                                               
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.description}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> р/день</div>
+                </div>
+            `;
+
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        4,
+        '.menu .container',
+        'menu__item',
+        'big'
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        8,
+        '.menu .container',
+        'menu__item'
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        7,
+        '.menu .container',
+        'menu__item'
+
+    ).render();
+    
 });
