a38392a59bb28c59e8cbf199632adc907c9b8934
artem <a.omukhov@gmail.com>
2021-10-11 19:53:13 +0300

    feat: slider

--- js/script.js
+++ js/script.js
@@ -187,39 +187,29 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        4,
-        '.menu .container',
-        'menu__item',
-        'big'
+    const getResource = async (url) => {                                        // Получение данных с базы для карточек
+        const response = await fetch(url);
 
-    ).render();
+        if (!response.ok) {                                                     // Если происходит ошибка сервера, то выходит исключение
+            throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        8,
-        '.menu .container',
-        'menu__item'
+        return await response.json();
+    };
 
-    ).render();
+    /* getResource('http://localhost:3000/menu')                                   // Перебор данных массива массивов из базы и получение свойств для карточек
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        }); */
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        7,
-        '.menu .container',
-        'menu__item'
-
-    ).render();
+    axios.get('http://localhost:3000/menu')                                         // Перебор данных массива массивов из базы и получение свойств для карточек при помощи axios
+        .then(card =>  {
+            card.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
     
     // Forms
 
@@ -232,14 +222,26 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {                                                   // Отправка данных с формы на сервер
+    const postData = async (url, data) => {                                     // Отправка данных в базу данных
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json;charset=utf-8'
+            },
+            body: data
+        });
+
+        return await response.json();
+    };
+
+    function bindPostData(form) {                                                   // Отправка данных с формы на сервер
         form.addEventListener('submit', (e) =>{
             e.preventDefault();
 
-            const statusMessage = document.createElement('img');
+            const statusMessage = document.createElement('img');                    // Формирование новой формы благодарности пользователя
             statusMessage.src = message.loading;
             statusMessage.style.cssText = `
                 display: block;
@@ -247,33 +249,23 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            });
+            //  Превращает formData в массив массивов и потом превращает в классический объект а затем в JSON
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));    
  
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
-        });
+
+        }); 
     }
 
     function showThanksModal(message) {                                         // Модальное окно благодарности пользователю
@@ -299,4 +291,201 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 2000);
     }
+
+    // Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),                  // Получение данных слайдера
+          slider = document.querySelector('.offer__slider'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
+
+    let slideIndex = 1;        
+    let offset = 0;                                                             // Изначальный индекс слайдера
+    
+    if (slides.length < 10) {                                                   // Условие при котором добавляется 0 цифрам в конечном количестве слайдов (Изначальное положение)
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    slidesField.style.width = 100 * slides.length + '%';                        // Ширина блока N (количество слайдов) * 100%   
+    slidesField.style.display = 'flex';                                         // Построение слайдов горизонтально
+    slidesField.style.transition = '0.5s all';                                  // Плавность анимации
+
+    slidesWrapper.style.overflow = 'hidden';                                    // Сокрытие лишних слайдов
+
+    slides.forEach(slide => {                                                   // Перебор слайдов и установка им одиннаковой ширины
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),                            // Получение списка точек
+          dots = [];
+
+    // Стили для индикатора карусели
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        // Стили для точек
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    next.addEventListener('click', () => {
+        // Если дошло до конца, то возврашается в начало
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {    
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {                                               // Прибавление 0 к цифрам
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    });
+
+    prev.addEventListener('click', () => {
+        // Если дошло до начала, то возврашается в конец
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
+        });
+    });
+
+    /* showSlides(slideIndex);
+
+    if (slides.length < 10) {                                                   // Условие при котором добавляется 0 цифрам в конечном количестве слайдов
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {                                                // Если выходит за пределы слайдера, возвращается к единице и наоборот
+            slideIndex = 1;
+        } 
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(slide => {                                               // Сокрытие других слайдов
+            slide.style.display = 'none';
+        });
+
+        slides[slideIndex - 1].style.display = 'block';                         // Показ нужного слайда
+
+        if (slides.length < 10) {                                               // Условие при котором добавляется 0 цифрам в временном количестве слайдов
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function plusSlides(n) {                                                    // Вызывает функцию показывания слайдов
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    }); */
+
+
 });
